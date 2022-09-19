b40fd72dfce353088cd2b0f84603b2d788d258cb
fhboswell <franklin.henry.boswell@gmail.com>
2022-09-19 16:39:01 -0700

    Added MAUI example, OneSignal.Xamarin beta dependency, and helper functions

---
+++ Samples/OneSignalApp.Sample/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace OneSignalApp.Sample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+		//Onesignal Helpers
+        SharedPush.Initialize();
+
+        MainPage = new AppShell();
+	}
+}
+
---
+++ Samples/OneSignalApp.Sample/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace OneSignalApp.Sample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
