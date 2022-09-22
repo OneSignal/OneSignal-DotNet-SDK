884eab2c89cba9a6d5dfb7ea6fc90da4068eea2b
fhboswell <franklin.henry.boswell@gmail.com>
2022-09-22 13:36:53 -0700

    Notification Service Extension setup

--- Samples/OneSignalNotificationServiceExtension/NotificationService.cs
+++ Samples/OneSignalNotificationServiceExtension/NotificationService.cs
@@ -1,39 +1,39 @@
 ﻿using System;
 using Foundation;
-using UIKit;
 using UserNotifications;
+using OneSignalSDK.Xamarin;
 
 namespace OneSignalNotificationServiceExtension
 {
-	[Register ("NotificationService")]
-	public class NotificationService : UNNotificationServiceExtension
-	{
-		Action<UNNotificationContent> ContentHandler { get; set; }
-		UNMutableNotificationContent BestAttemptContent { get; set; }
+    [Register("NotificationService")]
+    public class NotificationService : UNNotificationServiceExtension
+    {
+        Action<UNNotificationContent> ContentHandler { get; set; }
+        UNMutableNotificationContent BestAttemptContent { get; set; }
+        UNNotificationRequest ReceivedRequest { get; set; }
 
-		protected NotificationService (IntPtr handle) : base (handle)
-		{
-			// Note: this .ctor should not contain any initialization logic.
-		}
+        protected NotificationService(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
 
-		public override void DidReceiveNotificationRequest (UNNotificationRequest request, Action<UNNotificationContent> contentHandler)
-		{
-			ContentHandler = contentHandler;
-			BestAttemptContent = (UNMutableNotificationContent)request.Content.MutableCopy ();
+        public override void DidReceiveNotificationRequest(UNNotificationRequest request, Action<UNNotificationContent> contentHandler)
+        {
+            ReceivedRequest = request;
+            ContentHandler = contentHandler;
+            BestAttemptContent = (UNMutableNotificationContent)request.Content.MutableCopy();
 
-			// Modify the notification content here...
-			BestAttemptContent.Title = $"{BestAttemptContent.Title}[modified]";
+            (OneSignal.Default as OneSignalImplementation).DidReceiveNotificationExtensionRequest(request, BestAttemptContent, contentHandler);
+        }
 
-			ContentHandler (BestAttemptContent);
-		}
+        public override void TimeWillExpire()
+        {
+            // Called just before the extension will be terminated by the system.
+            // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
 
-		public override void TimeWillExpire ()
-		{
-			// Called just before the extension will be terminated by the system.
-			// Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+            (OneSignal.Default as OneSignalImplementation).ServiceExtensionTimeWillExpireRequest(ReceivedRequest, BestAttemptContent);
 
-			ContentHandler (BestAttemptContent);
-		}
-	}
+            ContentHandler(BestAttemptContent);
+        }
+    }
 }
-
