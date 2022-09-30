8ee69e8ad20af94dc673d559c987f22de732f31a
fhboswell <franklin.henry.boswell@gmail.com>
2022-09-30 10:19:15 -0700

    cleanup

--- Samples/OneSignalApp/exampledotnet/Platforms/Android/MainActivity.cs
+++ Samples/OneSignalApp/exampledotnet/Platforms/Android/MainActivity.cs
@@ -9,8 +9,5 @@ using OneSignalSDK.dotnet.Core;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
-{
-  
+{ 
 }
-
-
