2ab99b613484add88ba3800b968968556589f8db
fhboswell <franklin.henry.boswell@gmail.com>
2022-10-03 18:44:52 -0400

    Adding sample project UI

--- Samples/OneSignalApp/exampledotnet/MainPage.xaml.cs
+++ Samples/OneSignalApp/exampledotnet/MainPage.xaml.cs
@@ -1,25 +1,46 @@
-﻿namespace exampledotnet;
+﻿using System.Globalization;
+
+namespace exampledotnet;
 
 public partial class MainPage : ContentPage
 {
 	int count = 0;
+	string externalUserID = "";
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    private void OnRegisterForPush(object sender, EventArgs e)
+    {
+		// iOS only 
+		SharedPush.RegisterIOS();
+    }
+    private void OnSetExternalUserID(object sender, EventArgs e)
+    {
+        string externalUserID = ExternalUserID.Text;
+        SharedPush.SetExternalUserId(externalUserID);
+    }
+    private void OnRemoveExternalUserID(object sender, EventArgs e)
+    {
+        SharedPush.RemoveExternalUserId();
+    }
+    private void OnSendOutcomeWithValue(object sender, EventArgs e)
+    {
+        string uniqueOutcomeKeyField = OutcomeWithValueKeyField.Text;
+        float uniqueOutcomeValueField = float.Parse(OutcomeWithValueValueField.Text, CultureInfo.InvariantCulture.NumberFormat);
+        SharedPush.SendOutcomeWithValue(uniqueOutcomeKeyField, uniqueOutcomeValueField);
+    }
+    private void OnSendOutcome(object sender, EventArgs e)
+    {
+        string sendOutcome = SendOutcomeKey.Text;
+        SharedPush.SendOutcome(externalUserID);
+    }
+    private void OnSendUniqueOutcome(object sender, EventArgs e)
+    {
+        string sendOutcome = SendUniqueOutcomeKey.Text;
+        SharedPush.SendUniqueOutcome(externalUserID);
+    }
 }
 
 
