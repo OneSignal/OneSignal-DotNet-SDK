327d3ad9deac277b35eeddfb7aa2f450012f43a0
fhboswell <franklin.henry.boswell@gmail.com>
2022-11-01 10:11:31 -0700

    Final fixen and cleanup

--- Samples/OneSignalApp/exampledotnet/Platforms/Android/MainActivity.cs
+++ Samples/OneSignalApp/exampledotnet/Platforms/Android/MainActivity.cs
@@ -4,8 +4,8 @@ using Android.OS;
 
 namespace exampledotnet;
 
-using OneSignalSDK.dotnet;
-using OneSignalSDK.dotnet.Core;
+using OneSignalSDK.DotNet;
+using OneSignalSDK.DotNet.Core;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
