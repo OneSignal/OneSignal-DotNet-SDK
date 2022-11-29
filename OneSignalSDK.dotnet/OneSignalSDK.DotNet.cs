e1706704b434a42b7a32a8450b86225dc712907e
Brian Smith <briansmith@onesignal.com>
2022-11-29 15:12:34 -0500

    Updates from code review

--- OneSignalSDK.dotnet/OneSignalSDK.DotNet.cs
+++ OneSignalSDK.dotnet/OneSignalSDK.DotNet.cs
@@ -30,9 +30,9 @@ namespace OneSignalSDK.DotNet
             return null;
 #endif
 
-        }
+      }
 
-        internal static Exception NotImplementedInReferenceAssembly()
+      internal static Exception NotImplementedInReferenceAssembly()
       {
          return new NotImplementedException("This functionality is not implemented in the portable version of this assembly.  You should reference the NuGet package from your main application project in order to reference the platform-specific implementation.");
       }
