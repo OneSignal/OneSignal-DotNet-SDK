6477c74702efc8bbcc50b4d884ba4c62681ff256
Brian Smith <briansmith@onesignal.com>
2023-01-09 14:26:58 -0500

    Remove bin/obj dir commits

--- OneSignalSDK.DotNet.iOS.Binding/obj/Debug/net6.0-ios/LinkWithAttributes.cs
+++
@@ -1,4 +0,0 @@
-[assembly: ObjCRuntime.LinkWith ("OneSignal.xcframework", ForceLoad = true, Frameworks = "SystemConfiguration UserNotifications WebKit CoreGraphics UIKit OneSignalOutcomes OneSignalCore OneSignalExtension")]
-[assembly: ObjCRuntime.LinkWith ("OneSignalCore.xcframework", ForceLoad = true)]
-[assembly: ObjCRuntime.LinkWith ("OneSignalExtension.xcframework", ForceLoad = true, Frameworks = "UserNotifications OneSignalCore OneSignalOutcomes")]
-[assembly: ObjCRuntime.LinkWith ("OneSignalOutcomes.xcframework", ForceLoad = true, Frameworks = "OneSignalCore")]
