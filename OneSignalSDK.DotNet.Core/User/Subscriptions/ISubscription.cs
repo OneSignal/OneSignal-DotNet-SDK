b16b0c03db86aa9717fb7f79e245cea81aeb2671
Brian Smith <104789812+brismithers@users.noreply.github.com>
2023-02-27 15:02:09 -0500

    Update ISubscription.cs

--- OneSignalSDK.DotNet.Core/User/Subscriptions/ISubscription.cs
+++ OneSignalSDK.DotNet.Core/User/Subscriptions/ISubscription.cs
@@ -1,22 +1,23 @@
-﻿using System;
+using System;
 
-namespace OneSignalSDK.DotNet.Core.User.Subscriptions;
-
-/// <summary>
-/// A subscription.
-/// </summary>
-public interface ISubscription
+namespace OneSignalSDK.DotNet.Core.User.Subscriptions
 {
     /// <summary>
-    /// Event for when the subscription has changed.
+    /// A subscription.
     /// </summary>
-    event EventHandler<SubscriptionChangedEventArgs> Changed;
+    public interface ISubscription
+    {
+        /// <summary>
+        /// Event for when the subscription has changed.
+        /// </summary>
+        event EventHandler<SubscriptionChangedEventArgs> Changed;
 
-    /// <summary>
-    /// The unique identifier for this subscription. This will be an empty string
-    /// until the subscription has been successfully created on the backend and
-    /// assigned an ID. Use <see cref="Changed"/> to be notified when the
-    /// <see cref="Id"/> has been successfully assigned.
-    /// </summary>
-    string Id { get; }
+        /// <summary>
+        /// The unique identifier for this subscription. This will be an empty string
+        /// until the subscription has been successfully created on the backend and
+        /// assigned an ID. Use <see cref="Changed"/> to be notified when the
+        /// <see cref="Id"/> has been successfully assigned.
+        /// </summary>
+        string Id { get; }
+    }
 }
