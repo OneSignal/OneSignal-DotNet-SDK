73217e37bebf888180bf28920b2f8711c7eb33ea
Fadi George <fadii925@gmail.com>
2026-03-04 13:32:03 -0800

    chore: add retry logic to Android binary downloads

--- update_native_binaries.sh
+++ update_native_binaries.sh
@@ -57,24 +57,23 @@ update_ios_binaries() {
     move_ios_binary "../OneSignal-iOS-SDK/iOS_SDK/OneSignalSDK/OneSignal_LiveActivities/OneSignalLiveActivities.xcframework"
 }
 
-# After running, check for 404 or other errors in the terminal
 update_android_binaries() {
 	echo "Updating Android binaries to version ${ANDROID_NATIVE_VERSION}"
     curl https://repo1.maven.org/maven2/com/onesignal/core/${ANDROID_NATIVE_VERSION}/core-${ANDROID_NATIVE_VERSION}.aar \
          --output OneSignalSDK.DotNet.Android.Core.Binding/Jars/core-release.aar \
-         --fail
+         --fail --retry 5 --retry-delay 30 --retry-all-errors
 
     curl https://repo1.maven.org/maven2/com/onesignal/notifications/${ANDROID_NATIVE_VERSION}/notifications-${ANDROID_NATIVE_VERSION}.aar \
          --output OneSignalSDK.DotNet.Android.Notifications.Binding/Jars/notifications-release.aar \
-         --fail
+         --fail --retry 5 --retry-delay 30 --retry-all-errors
 
     curl https://repo1.maven.org/maven2/com/onesignal/location/${ANDROID_NATIVE_VERSION}/location-${ANDROID_NATIVE_VERSION}.aar \
          --output OneSignalSDK.DotNet.Android.Location.Binding/Jars/location-release.aar \
-         --fail
+         --fail --retry 5 --retry-delay 30 --retry-all-errors
 
     curl https://repo1.maven.org/maven2/com/onesignal/in-app-messages/${ANDROID_NATIVE_VERSION}/in-app-messages-${ANDROID_NATIVE_VERSION}.aar \
          --output OneSignalSDK.DotNet.Android.InAppMessages.Binding/Jars/in-app-messages-release.aar \
-         --fail
+         --fail --retry 5 --retry-delay 30 --retry-all-errors
 }
 
 # Only update iOS if the version was provided
